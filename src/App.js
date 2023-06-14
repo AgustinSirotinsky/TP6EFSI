c0360c523bf11661a5a7cff262a91e5eb672fd28
AGUSTÍN SIROTINSKY <46645963@ort>
2023-06-14 08:47:56 -0300

    fetch anda pero sin useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React,{useEffect, useState} from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getList } from './components/list';
 
 function App() {
-  const [list, setList] = useState ([]);
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://countriesnow.space/api/v0.1/countries/flag/images')
+      .then(data => data.json())
+      .then(res=>(
+        setCountries(res.data)
+      ));
+  }
+  )
+
+  useEffect(() => {
+    let mounted = true;
+    getList()
+      .then(items => {
+        if(mounted) {
+          setList(items)
+        }
+      })
+    return () => mounted = false;
+  }, [])
+
+  return(
+    <div className="wrapper">
+     <h1>Lista :D</h1>
+     <ul>
+       {console.log(list)}
+     </ul>
+   </div>
+  )
 }
 
 export default App;
--- src/components/randomflag.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function RandomFlag(props){
-    let x = Math.random()
-    
-}
-
-export default RandomFlag
